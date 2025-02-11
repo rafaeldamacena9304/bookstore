5fa8ad61a7ede3c1c8ca9f7c0f8ca048f86466d3
Rafael <rafaeldamacena9304@gmail.com>
2025-02-11 11:31:54 -0300

    fix: tests errors

--- product/tests/test_viewsets/test_category_viewset.py
+++ product/tests/test_viewsets/test_category_viewset.py
@@ -20,7 +20,7 @@ class CategoryViewSet(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         category_data = json.loads(response.content)
 
-        self.assertEqual(category_data[0]["title"], self.category.title)
+        self.assertEqual(category_data["results"][0]["title"], self.category.title)
 
     def test_create_category(self):
         data = json.dumps({"title": "technology"})
@@ -36,3 +36,15 @@ class CategoryViewSet(APITestCase):
         created_category = Category.objects.get(title="technology")
 
         self.assertEqual(created_category.title, "technology")
+
+    def test_delete_category(self):
+        category_id = self.category.id
+
+        response = self.client.delete(
+            reverse("category-detail", kwargs={"version": "v1", "pk": category_id})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(Category.DoesNotExist):
+            Category.objects.get(id=category_id)
--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -1,43 +1,66 @@
 import json
 
-from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient, APITestCase
-from rest_framework.views import status
 
-from order.factories import UserFactory
+from django.urls import reverse
+
 from product.factories import CategoryFactory, ProductFactory
-from product.models import Product
+from order.factories import UserFactory
+from product.models.product import Product
 
 
 class TestProductViewSet(APITestCase):
+
     client = APIClient()
 
     def setUp(self):
         self.user = UserFactory()
-        token = Token.objects.create(user=self.user)  # added
-        token.save()  # added
+        token = Token.objects.create(user=self.user)
+        token.save()
 
         self.product = ProductFactory(
             title="pro controller",
             price=200.00,
         )
 
-    def test_get_all_product(self):
-        token = Token.objects.get(user__username=self.user.username)  # added
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)  # added
+    def test_get_all_products(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
         response = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        product_data = json.loads(response.content)
+        self.assertEqual(product_data["results"][0]["title"], self.product.title)
+        self.assertEqual(product_data["results"][0]["price"], self.product.price)
+        self.assertEqual(product_data["results"][0]["active"], self.product.active)
+
+    def test_get_single_product(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
+        product_id = self.product.id
+
+        response = self.client.get(
+            reverse("product-detail", kwargs={"version": "v1", "pk": product_id})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         product_data = json.loads(response.content)
 
-        self.assertEqual(product_data[0]["title"], self.product.title)
-        self.assertEqual(product_data[0]["price"], self.product.price)
-        self.assertEqual(product_data[0]["active"], self.product.active)
+        # import pdb; pdb.set_trace()
+
+        self.assertEqual(product_data["title"], self.product.title)
+        self.assertEqual(product_data["price"], self.product.price)
 
     def test_create_product(self):
         token = Token.objects.get(user__username=self.user.username)
         self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
         category = CategoryFactory()
         data = json.dumps(
             {"title": "notebook", "price": 800.00, "categories_id": [category.id]}
@@ -55,3 +78,20 @@ class TestProductViewSet(APITestCase):
 
         self.assertEqual(created_product.title, "notebook")
         self.assertEqual(created_product.price, 800.00)
+
+    def test_delete_product(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
+        product_id = self.product.id
+
+        response = self.client.delete(
+            reverse("product-detail", kwargs={"version": "v1", "pk": product_id})
+        )
+
+        # import pdb; pdb.set_trace()
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(Product.DoesNotExist):
+            Product.objects.get(id=product_id)
